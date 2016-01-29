5e119f51f27f1f8a80f442b150f24c43f16b53aa
Andrew Gordon <abgordon@colorado.edu>
2016-01-29 01:01:10 -0700

    fixed python client endpoint

--- python_ws/client.py
+++ python_ws/client.py
@@ -6,23 +6,23 @@ import time
 import logging
 
 logging.basicConfig()
-
 ip = "104.154.67.221"
 port = "8080"
+endpoint = "echo"
 
 class WSClient():
 
     def __init__(self):
         websocket.enableTrace(False)
-        print "Connecting to ws://" + ip + ":" + port +"..."
-        self.ws = websocket.WebSocketApp("ws://" + ip + ":" + port,
+        print "Connecting to ws://" + ip + ":" + port + "/" + endpoint + "..."
+        self.ws = websocket.WebSocketApp("ws://" + ip + ":" + port + "/" + endpoint,
         on_message = self.on_message,
         on_error = self.on_error,
         on_close = self.on_close)
         self.ws.on_open = self.on_open
+        print "successfully connected."
         self.ws.run_forever()
 
-        self.ws.send("Successfully connected.")
 
     def on_message(self, ws, message):
         print "rcvd:", message, ws
@@ -38,7 +38,9 @@ class WSClient():
 
         while True:
             time.sleep(1)
-            self.ws.send("Test msg")
+            msg = "test msg"
+            print msg #see whats going out
+            self.ws.send(msg)
 
 
 if __name__ == "__main__":
