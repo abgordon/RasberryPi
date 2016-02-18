ec95c1c3d41158516f7024f15070723619c85e92
Andrew Gordon <abgordon@colorado.edu>
2016-02-17 21:08:13 -0700

    moved main loop

--- python_ws/DHT22_client.py
+++ python_ws/DHT22_client.py
@@ -6,7 +6,7 @@ import logging
 import time
 import atexit
 import pigpio
-
+import DHT22
 
 logging.basicConfig()
 ip = "104.197.37.118"
@@ -39,48 +39,36 @@ class WSClient():
 
     def on_open(self, ws):
         print "connected"
+        # Intervals of about 2 seconds or less will eventually hang the DHT22.
+        INTERVAL=3
+
+        pi = pigpio.pi()
+
+        s = DHT22.sensor(pi, 22, LED=16, power=8)
+
+        r = 0
+
+        next_reading = time.time()
+
+        while True:
+
+          r += 1
+
+          s.trigger()
+
+          time.sleep(0.2)
+
+          print("{} {} {} {:3.2f} {} {} {} {}".format(r, s.humidity(), s.temperature(), s.staleness(),s.bad_checksum(), s.short_message(), s.missing_message(),s.sensor_resets()))
+
+          next_reading += INTERVAL
+
+          time.sleep(next_reading-time.time()) # Overall INTERVAL second polling.
+
+          s.cancel()
+
+          pi.stop()
 
 
 
 if __name__ == "__main__":
-
-   import time
-
-   import pigpio
-
-   import DHT22
-
    client = WSClient()
-
-
-   # Intervals of about 2 seconds or less will eventually hang the DHT22.
-   INTERVAL=3
-
-   pi = pigpio.pi()
-
-   s = DHT22.sensor(pi, 22, LED=16, power=8)
-
-   r = 0
-
-   next_reading = time.time()
-
-   while True:
-
-      r += 1
-
-      s.trigger()
-
-      time.sleep(0.2)
-
-      print("{} {} {} {:3.2f} {} {} {} {}".format(
-         r, s.humidity(), s.temperature(), s.staleness(),
-         s.bad_checksum(), s.short_message(), s.missing_message(),
-         s.sensor_resets()))
-
-      next_reading += INTERVAL
-
-      time.sleep(next_reading-time.time()) # Overall INTERVAL second polling.
-
-   s.cancel()
-
-   pi.stop()
