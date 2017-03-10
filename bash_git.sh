d108bbacbe6df168eb327b7c276b78f26d6f0f83
Andrew Gordon <abebie001c@cotnpl-gf7g8wp.cable.comcast.com>
2017-03-10 13:42:31 -0700

    testing tags

---
+++ bash_git.sh
@@ -0,0 +1,12 @@
+LATEST_TAG=$(git describe --tags --abbrev=0)
+CURRENT_REVISION=$(git describe)
+FILES_CHANGED=$(git diff --name-only HEAD $LATEST_TAG)
+
+for var in "${FILES_CHANGED[@]}"
+do
+        if [[ "$var" == "$SITE" ]]
+        then
+                echo $var $SITE
+                VALID=1
+        fi
+done
